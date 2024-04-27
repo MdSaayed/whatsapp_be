8b25f58e87dd1e5c3886e8c699e9549916093034
MdSaayed <sayedkhan1786.com>
2024-04-27 14:11:39 +0600

    setup cors-env, adding middlwares:morgan helmet

--- src/app.js
+++ src/app.js
@@ -1,9 +1,52 @@
 import express from "express";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import helmet from "helmet";
+import mongoSanitize from "express-mongo-sanitize";
+import cookeParser from "cookie-parser";
+import compression from "compression";
+import fileUpload from "express-fileupload";
+import cors from "cors";
 
+// dotEnv config
+dotenv.config();
 
 // create express app
 const app = express();
 
+// morgan
+if (process.env.NODE_ENV !== "production") {
+app.use(morgan("dev"));
+}
+
+// helmet
+app.use(morgan());
+
+// pase json request body
+app.use(express.json());
+
+// sanitize request data
+app.use(mongoSanitize());
+
+// enable cookie parser
+app.use(cookeParser());
+
+// gzip compression
+app.use(compression());
+
+// file upload
+app.use(fileUpload(
+    {
+        useTempFiles: true,
+    }
+))
+
+// cors
+app.use(cors());
+
+app.use(express.urlencoded({ extended: true }));
+ 
+ 
 app.get("/", (req, res) => {
    res.send("Hello from server...")
 })
--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
-import dotenv from "dotenv";
 import app from "./app.js";
 
-// dotEnv config
-dotenv.config();
+
 
 // dotEnv variables
 const PORT = process.env.PORT || 8001; 
+console.log(process.env.NODE_ENV)
 
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`); 
